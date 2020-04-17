f6fa56e5441626332ae25dc70db63ef49713cc6e
Marek Behún <marek.behun@nic.cz>
2020-04-17 20:00:17 +0200

    arm: mvebu: configs: turris_mox: give malloc 64 MiB of memory
    Set CONFIG_SYS_MALLOC_LEN to 64 MiB on Turris Mox. Minimum amount of RAM
    is 512 MiB, so this is not a problem.
    
    Signed-off-by: Marek Behún <marek.behun@nic.cz>

--- include/configs/turris_mox.h
+++ include/configs/turris_mox.h
@@ -39,7 +39,7 @@
 /*
  * Size of malloc() pool
  */
-#define CONFIG_SYS_MALLOC_LEN	(4 << 20) /* 4MiB for malloc() */
+#define CONFIG_SYS_MALLOC_LEN	(64 << 20) /* 64MiB for malloc() */
 
 /*
  * Other required minimal configurations
