05fb99ddb4655a398226ed631084145fd841728e
Pali Rohár <pali@kernel.org>
2022-08-27 19:44:43 +0200

    board_f: Add support for CONFIG_OF_BOARD_FIXUP for XIP images
    When U-Boot is running from flash memory (execute in place) then
    gd->fdt_blob before relocation points to read-only flash memory.
    
    So U-Boot calls board_fix_fdt() with read-only gd->fdt_blob pointer which
    cause immediate CPU crash when callback is trying to modify gd->fdt_blob.
    
    Fix this issue by introducing a new config option OF_INITIAL_DTB_READONLY
    which moves fix_fdt callback after the reloc_fdt callback. This makes
    CONFIG_OF_BOARD_FIXUP working also if U-Boot before relocation is not
    running from read/write (S)RAM memory.
    
    This is required for mpc85xx boards when booting from flash NOR.
    
    Signed-off-by: Pali Rohár <pali@kernel.org>

--- common/board_f.c
+++ common/board_f.c
@@ -946,7 +946,7 @@ static const init_fnc_t init_sequence_f[] = {
 	 *  - board info struct
 	 */
 	setup_dest_addr,
-#ifdef CONFIG_OF_BOARD_FIXUP
+#if defined(CONFIG_OF_BOARD_FIXUP) && !defined(CONFIG_OF_INITIAL_DTB_READONLY)
 	fix_fdt,
 #endif
 #ifdef CFG_PRAM
@@ -962,6 +962,10 @@ static const init_fnc_t init_sequence_f[] = {
 	reserve_board,
 	reserve_global_data,
 	reserve_fdt,
+#if defined(CONFIG_OF_BOARD_FIXUP) && defined(CONFIG_OF_INITIAL_DTB_READONLY)
+	reloc_fdt,
+	fix_fdt,
+#endif
 	reserve_bootstage,
 	reserve_bloblist,
 	reserve_arch,
@@ -972,7 +976,9 @@ static const init_fnc_t init_sequence_f[] = {
 	setup_bdinfo,
 	display_new_sp,
 	INIT_FUNC_WATCHDOG_RESET
+#if !defined(CONFIG_OF_BOARD_FIXUP) || !defined(CONFIG_OF_INITIAL_DTB_READONLY)
 	reloc_fdt,
+#endif
 	reloc_bootstage,
 	reloc_bloblist,
 	setup_reloc,
